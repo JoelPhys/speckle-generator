e81a697c3f6e021fe3a08ddb62425c2e5a6a804f
JoelPhys <jrhirst100@aol.com>
2025-01-31 09:08:14 +0000

    Updated example 1.

--- src/examples/example1_generate_and_save_pattern.py
+++ src/examples/example1_generate_and_save_pattern.py
@@ -1,7 +1,7 @@
 import speckle
 
 # Generate a Speckle pattern and print some basic statitics.
-pattern = speckle.Pattern(image_width=512,image_height=512, mean_radius=6, spacing=12, variability=0.6,stddev_size=1.0,gray_level=256,stddev_smooth=1.0)
+pattern = speckle.Pattern(image_width=2000,image_height=2000, mean_radius=6, spacing=12, variability=0.6,stddev_size=1.0,gray_level=256,stddev_smooth=1.0)
 speckle_pattern = pattern.generate()
 pattern.statistics()
 pattern.levels_histogram()
